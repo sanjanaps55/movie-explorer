bbaba2d829ec67fc405e68e56c96127030164599
sanjanaps55 <pssanjana55@gmail>
2025-12-07 22:13:39 +0530

    chnages made to fix env

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-BASE_URL=process.env.BASE_URL
-API_KEY=process.env.API_KEY
+const BASE_URL=import.meta.env.VITE_BASE_URL
+const API_KEY=import.meta.env.VITE_API_KEY
 export const getPopularMovies =async()=>{
     const response =await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data= await response.json()
